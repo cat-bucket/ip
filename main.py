8b9acd2446789b199c288b69ec7a7803b3cee6a9
cat-bucket <gitbit843@gmail.com>
2024-09-08 12:20:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-
 import subprocess
+import os
 
 def print_banner():
     banner = '''
@@ -18,9 +18,14 @@ def call_script(script_name):
         print(f"Error occurred while calling {script_name}: {e}")
 
 if __name__ == '__main__':
-    print_banner()
-
-    # 显示选项并根据用户输入选择性地调用文件
-    user_input = input("1.模糊域名FUZZ")
-    if user_input == '1':
-        call_script('ip.py')  # 调用 ip.py 文件
+    while True:  # 添加循环以允许重新调用
+        print_banner()
+        user_input = input("1.模糊域名FUZZ\n2.退出\n请输入选项: ")
+        if user_input == '1':
+            call_script('ip.py')  # 调用 ip.py 文件
+        elif user_input == '2':
+            print("退出程序。")
+            break  # 退出循环
+        else:
+            print("无效选项，请重试。")
+        os.system('cls' if os.name == 'nt' else 'clear')  # 清空屏幕
