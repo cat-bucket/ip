1a077051a33492234f6a7bb617bba2885c04b660
cat-bucket <gitbit843@gmail.com>
2024-09-08 14:38:17 +0800

    更新

---
+++ cat.py
@@ -0,0 +1,45 @@
+import os
+import time
+
+# 猫与爱心的帧列表
+frames = [
+    r"""
+     /\_/\  
+    ( o.o ) 
+     > ^ <  ♥
+    """,
+    r"""
+     /\_/\  
+    ( o.o ) 
+     > ^ <   
+    """,
+    r"""
+     /\_/\  
+    ( o.o ) ♥
+     > ^ <  
+    """,
+    r"""
+     /\_/\  
+    ( o.o ) 
+     > ^ <  
+    """,
+]
+
+def clear_screen():
+    # 清除终端屏幕
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def display_animation():
+    while True:
+        for frame in frames:
+            clear_screen()
+            print(frame)
+            time.sleep(0.5)  # 控制帧之间的时间间隔
+
+if __name__ == "__main__":
+    try:
+        display_animation()
+    except KeyboardInterrupt:
+        # 用户按下 Ctrl+C 时退出
+        clear_screen()
+        print("动画已停止。")
