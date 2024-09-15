6a2a79ffa937e33ef8cb8c544c561ca2fb75fbbd
cat-bucket <gitbit843@gmail.com>
2024-09-15 14:42:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ import subdomain_brute_module  # 导入子域名爆破模块
 GREEN = "\033[92m"
 RED = "\033[91m"
 
+# 清屏
 os.system('cls' if os.name == 'nt' else 'clear')
 
 def print_banner():
+    """打印程序横幅"""
     banner = f"""{GREEN}
   ____      ____    _    _    ____  _  __  _____  _____  
  / ___|    | __ )  | |  | |  / ___|| |/ / | ____||_   _| 
@@ -19,20 +21,29 @@ def print_banner():
     print(banner)
 
 def call_script(script_name):
+    """调用指定的脚本"""
     try:
         subprocess.run(['python', script_name], check=True)
     except subprocess.CalledProcessError as e:
         print(f"{RED}Error occurred while calling {script_name}: {e}")
 
-if __name__ == '__main__':
+def main_menu():
+    """主菜单"""
     while True:
         print_banner()
-        user_input = input(f"{GREEN}1.模糊域名FUZZ\n2.子域名爆破\n3.退出\n请输入选项(填入1,2,3……): ")
+        print(f"{GREEN}===============================")
+        print(f"{GREEN}  1. 模糊域名 FUZZ")
+        print(f"{GREEN}  2. 子域名爆破")
+        print(f"{GREEN}  3. 退出")
+        print(f"{GREEN}===============================")
+        
+        user_input = input(f"{GREEN}请输入选项 (填入 1, 2, 3): ")
+        
         if user_input == '1':
             call_script('ip.py')  # 调用 ip.py 文件
         elif user_input == '2':
             target_domain = input(f"{GREEN}请输入目标域名 (例如 example.com): ")
-            subdomain_brute_module.select_mode_and_brute(target_domain)  # 直接调用子域名爆破模块中的函数
+            subdomain_brute_module.select_mode_and_brute(target_domain)  # 调用子域名爆破模块中的函数
         elif user_input == 'linzixuan':
             call_script('cat.py')
         elif user_input == '3':
@@ -40,4 +51,9 @@ if __name__ == '__main__':
             break
         else:
             print(f"{RED}无效选项，请重试")
-        os.system('cls' if os.name == 'nt' else 'clear')  # 清空屏幕
+        
+        # 清空屏幕
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+if __name__ == '__main__':
+    main_menu()
